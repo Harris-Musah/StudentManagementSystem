50097520e28dd0d8c76edf25be221fb645c5b4e0
Harris-Musah <hrrsabdallah123@gmail.com>
2024-07-27 23:28:09 +0000

    This is a school project on student management system. There are few changes required.

---
+++ src/studentmanagementsystem/StudentManagementSystem.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentmanagementsystem;
+
+import SystemManagement.LoginPage;
+
+/**
+ *
+ * @author Harris Abdallah Musa
+ */
+public class StudentManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new LoginPage().setVisible(true);
+    }
+    
+}
